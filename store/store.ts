58d63fc66616fc2d709ab1b4510846240697b754
Azeem <bljazeem@gmail.com>
2025-03-08 04:58:04 +0100

    Chore: more code cleanup

--- store/store.ts
+++ store/store.ts
@@ -1,18 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import businessGeneral from './features/businessGeneral';
-import businessContact from './features/businessContact';
-import businessSocials from './features/businessSocials';
-import businessLinks from './features/businessLink';
-import campaing from './features/campaign';
 
 export const store = configureStore({
-  reducer: {
-    businessGeneral,
-    businessContact,
-    businessSocials,
-    businessLinks,
-    campaing,
-  },
+  reducer: {},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
