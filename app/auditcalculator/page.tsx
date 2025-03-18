196eeb4b526ee38a89d7908d33f3b18b8fd2993f
Azeem <bljazeem@gmail.com>
2025-03-18 09:20:19 +0100

    ssoId wrapped

--- app/auditcalculator/page.tsx
+++ app/auditcalculator/page.tsx
@@ -1,12 +1,20 @@
 'use client';
 
-import React from 'react';
+import React, { Suspense } from 'react';
 import Dashboard from './Dashboard';
 import { useSearchParams } from 'next/navigation';
 
-export default function Page() {
+function SsoIdWrapper() {
   const searchParams = useSearchParams();
   const ssoId = searchParams.get('ssoId');
 
   return <Dashboard ssoId={ssoId ?? ''} />;
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <SsoIdWrapper />
+    </Suspense>
+  );
 }
