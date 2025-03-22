0e6c006c642221324a3fe93347ef90bea2051511
Valentine Onyekachi <valentine@levelupesg.co>
2025-03-22 09:50:30 +0100

    integrated stripe

---
+++ app/paypal-payment/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import PayPalPayment from "./Paypal";
+
+export default function Page() {
+  return <PayPalPayment />;
+}
---
+++ app/stripe-payment/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import StripePayment from "./Stripe";
+
+export default function Page() {
+  return <StripePayment />;
+}
